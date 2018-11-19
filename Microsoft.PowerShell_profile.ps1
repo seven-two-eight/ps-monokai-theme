98025b860b4d6bddb1b4aebcefbefc76ce581089
Dybdatech <29250734+dybdaltech@users.noreply.github.com>
2018-11-19 14:35:13 +0100

    Checking for PSReadline version 2.0.0

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -8,15 +8,31 @@ function prompt {
 
 $Host.PrivateData.DebugBackgroundColor = "DarkGray"
 $Host.PrivateData.ErrorBackgroundColor = "DarkGray"
-Set-PSReadLineOption -Colors @{
+
+
+$promptColors = @{
     'Command' = 'White'
     'Comment' = 'DarkGreen'
-    'Keyword' = 'Blue'
+    'Keyword' = 'DarkCyan'
     'Member' = 'Cyan'
     'Number' = 'Yellow'
     'Operator' = 'Magenta'
     'Parameter' = 'Cyan'
     'String' = 'Yellow'
     'Type' = 'Green'
-    'Variable' = 'White'
+    'Variable' = 'DarkCyan'
+}
+if((Get-Module -Name "PSReadline").Version -gt 1.0.0) {
+    Set-PSReadLineOption -Colors $promptColors
+} else {
+    Set-PSReadlineOption -TokenKind Command   -ForegroundColor White
+    Set-PSReadlineOption -TokenKind Comment   -ForegroundColor DarkGreen
+    Set-PSReadlineOption -TokenKind Keyword   -ForegroundColor Blue
+    Set-PSReadlineOption -TokenKind Member    -ForegroundColor Cyan
+    Set-PSReadlineOption -TokenKind Number    -ForegroundColor Yellow
+    Set-PSReadlineOption -TokenKind Operator  -ForegroundColor Magenta
+    Set-PSReadlineOption -TokenKind Parameter -ForegroundColor Cyan
+    Set-PSReadlineOption -TokenKind String    -ForegroundColor Yellow
+    Set-PSReadlineOption -TokenKind Type      -ForegroundColor Green
+    Set-PSReadlineOption -TokenKind Variable  -ForegroundColor White
 }
