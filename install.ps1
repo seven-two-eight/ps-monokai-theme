3b46124a827d68d9e287e6b7792e77159da0ca94
Zafer Balkan <zafer@zaferbalkan.com>
2019-08-06 18:19:58 +0300

    Modified management of registry
    First created backups for registry entries, then imported the `.reg` file. Since the exception management does not include any solution but just prints out the error message, used the existing `reg` command.

--- install.ps1
+++ install.ps1
@@ -1,16 +1,29 @@
-$StartParams = @{
-    FilePath = "$Env:SystemRoot\regedit.exe"
-    ArgumentList = '/s','.\colors.reg'
-    Verb = 'RunAs'
-    PassThru = $True
-    Wait = $True
+# Get Script Path
+try {
+    if($PSVersionTable.PSVersion.Major -lt 3) {
+        $scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
+    }
+    $scriptPath = $PSScriptRoot
+    if (!$scriptPath) {
+        if ($psISE) {
+            $scriptPath = Split-Path -Parent -Path $psISE.CurrentFile.FullPath
+        } else {
+            Write-Host -ForegroundColor Red "Cannot resolve script file's path"
+            exit 1
+        }
+    }
+} catch {
+    Write-Host -ForegroundColor Red "Caught Exception: $($Error[0].Exception.Message)"
+    exit 2
 }
-$Proc = Start-Process @StartParams
 
-If ($Proc.ExitCode -ne 0) {
-    Write-Host "Failed importing registry! Exit code: $($Proc.ExitCode)" 
-    Exit
-}
+# Backup colors
+reg export 'HKCU:\Console\Windows PowerShell' "$scriptPath\backup_WindowsPowershell.reg"
+reg export HKCU:\Console\%SystemRoot%_System32_WindowsPowerShell_v1.0_powershell.exe "$scriptPath\backup_WindowsPowershell_sys32.reg"
+reg export HKCU:\Console\%SystemRoot%_SysWOW64_WindowsPowerShell_v1.0_powershell.exe "$scriptPath\backup_WindowsPowershell_sysWOW64.reg"
+
+# Add colors
+reg import "$scriptPath\colors.reg"
 
 $src = "Microsoft.PowerShell_profile.ps1"
 $des = "$PROFILE.CurrenUserCurrentHost"
